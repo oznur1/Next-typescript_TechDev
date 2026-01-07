8e26525e455e2eadac598ead06b35b2d7ddf8aa1
oznur1 <oznuroncel10@gmail.com>
2026-01-07 15:32:05 +0300

    projenin iskelet yapısı oluşturuldu

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className="text-5xl text-blue-700">
+      Home
+    </div>
+  )
+}
+
+export default HomePage
+
+
+//localhost:3000
+//localhost:3000/product
+//localhost:3000/product/product-details
+//localhost:3000/product/product-details/1
